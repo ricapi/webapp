d7127287c23ba6a1984382e1384509e950c7d4b4
Ricardo Pineda <rikaldero@hotmail.com>
2021-09-08 19:58:13 -0500

    creación de una búsqueda para usuarios en twitter

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import twitter_tools as tt
 
 #Crear instancia de flask
 #le damos instancia de flask a Api
@@ -10,8 +11,13 @@ api = Api(app)
 class Hello(Resource):
     def get(self, name):
         return {"hello": name}
+class Search(Resource):
+    def get(self, name):
+        person = tt.twitter(name)
+        return person
 
 #Recurso a la API y asociación con ruta
+api.add_resource(Search, '/search/<name>')
 api.add_resource(Hello, '/hello/<name>')
 
 #correr aplicación
---
+++ twitter_tools.py
@@ -0,0 +1,44 @@
+from models import Person
+import tweepy
+from datetime import datetime, date, time, timedelta
+
+people = []
+
+def twitter(screen_name):
+    for person in people:
+        if person.screen_name == screen_name:
+            return person.__dict__
+
+    api = get_api()
+
+    user = None
+    try:
+        user = api.get_user(screen_name)
+    except tweepy.error.TweepError:
+        return {"Error": "Username Not Found"}
+    tweets_count = user.statuses_count
+    account_created_date = user.created_at
+    followers_count = user.followers_count
+    days = (datetime.utcnow() - account_created_date).days
+
+    timeline = []
+    for status in tweepy.Cursor(api.user_timeline, screen_name='@' + screen_name).items():
+        timeline.append({"text": status._json["text"], "created_at": status._json["created_at"]})
+
+    avg = float(tweets_count) / float(days)
+    person = Person(screen_name, tweets_count, followers_count, days, timeline, avg)
+    people.append(person)
+    return person.__dict__
+
+
+def get_api():
+    consumer_key = 'YOUR CONSUMER KEY'
+    consumer_secret = 'YOUR CONSUMER SECRET'
+
+    access_token = 'YOUR ACCESS TOKEN'
+    access_token_secret = 'YOUR ACCESS TOKEN SECRET'
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    return tweepy.API(auth)
